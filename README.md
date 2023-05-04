56991e045a14e922ebcbf0a07a06852b00f6c4aa
maxine <maxbeckie@tutanota.com>
2023-05-04 06:30:04 -0700

    config option works, but kinda janky
      -also verbose mode added with env_logger

--- README.md
+++ README.md
@@ -14,10 +14,13 @@ I like to create a fish alias.
 ```fish 
 alias --save nim="/path/to/nimnimnim/target/release/nimnimnim"
 ```
-The only thing currently working is calculating nim sums according to the rules (1) no pile splits and (2) can take as many matches from one pile as desired. 
+You can calculate nim sums, eg.
 ```sh
 nim -s "1 3 5 7"
 ```
-returns `*0`. 
-
+returns `*0`. The default rules are (1) can take any amount from one pile, and (2) taking never splits a pile. These can be changed according to the `-c` option. The config file should contain a `NimRule` struct in RON format. See the [RON documentation](https://github.com/ron-rs/ron) and the [nimlib documentation](https://docs.rs/nimlib/0.1.1/nimlib/index.html) for more information. The config option is still pretty janky, it will be improved in the future. Example:
+```sh
+echo "NimRule(take: Any, split: Always)" > config.ron
+nim -c config.ron -s "1 3 5 7"
+```
 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+use std::ffi::OsString;
 use std::path::PathBuf;
 use std::process;
 
@@ -5,37 +7,18 @@ use anyhow::Result;
 use clap::{arg, command, value_parser, ArgAction, Command};
 use nimlib::{moves, NimAction, NimGame, NimRule, Split, Stack, TakeSize};
 
-const RULES: [NimRule; 1] = [NimRule {
-    take: TakeSize::Any,
-    split: Split::Never,
-}];
+use log::{debug, error, info, log_enabled, Level};
 
-const STACKS: [Stack; 4] = [Stack(1), Stack(3), Stack(5), Stack(7)];
+use serde::{Deserialize, Serialize};
 
-fn make_move_woo(game: &mut NimGame) {
-    let mov = moves::calculate_legal_moves(&STACKS, &RULES, (0, 0))
-        .into_iter()
-        .filter(|mov| match mov {
-            NimAction::Take(take) => true,
-            _ => false,
-        })
-        .take(1)
-        .collect::<Vec<NimAction>>()
-        .pop()
-        .unwrap();
-
-    unsafe {
-        moves::apply_move_unchecked(game, &mov);
-    }
-}
+use env_logger::Env;
 
 fn main() {
-    env_logger::init();
 
     process::exit(match run() {
         Ok(()) => 0,
         Err(e) => {
-            eprintln!("Fatal: {e}");
+            eprintln!("RKO: {e}");
             1
         }
     });
@@ -45,60 +28,88 @@ fn run() -> Result<()> {
     let matches = command!()
         .arg(
             arg!(
-                -c --config <FILE> "set custom rules in RON format (https://github.com/ron-rs/ron). Default is:
-                         (take: TakeSize::Any,  split: Split::Never)
-                for more information see the nimlib documentation on the NimRule struct."
+                -c --config <FILE> "Set custom rules in RON format. If no file is specified, a default config will be generated."
             )
             .required(false)
-            .value_parser(value_parser!(PathBuf)),
+            .value_parser(value_parser!(PathBuf))
         )
-        .arg(arg!(
-            -d --debug ... "Turn debugging information on"
-        ))
-        .subcommand(
-            Command::new("test")
-                .about("does testing things")
-                .arg(arg!(-l --list "lists test values").action(ArgAction::SetTrue)),
-        )
-       .arg(
+        .arg(
             arg!(
-                -s --sum <PILE> "return nim sum of piles, eg. nimnimnim -s \"1 3 5 7\" returns 0"
+                -s --sum <PILES> "return nim sum of piles according to config, eg. nimnimnim -s \"1 3 5 7\" returns *0 with default config."
             )
             .required(false)
             .value_parser(value_parser!(String)),
         )
+        .arg(
+            arg!(
+                -v --verbose ... "Once enables warning log levels, twice info log levels."
+            )
+            .required(false)
+        )
         .get_matches();
+
+    let mut rule: NimRule = NimRule {
+        take: TakeSize::Any,
+        split: Split::Never,
+    };
+
+    // determine verbosity level
+    match matches.get_one::<u8>("verbose").unwrap() {
+        0 => env_logger::Builder::from_env(Env::default().default_filter_or("error")).init(),
+        1 => env_logger::Builder::from_env(Env::default().default_filter_or("warn")).init(),
+        _ => env_logger::Builder::from_env(Env::default().default_filter_or("info")).init(),
+    }
 
     // config
     if let Some(config_path) = matches.get_one::<PathBuf>("config") {
-        println!("Value for config: {}", config_path.display());
+        // parse rules from config
+        rule = ron::from_str(&match std::fs::read_to_string(config_path) {
+            Ok(ron_string) => {
+                info!("using config file provided");
+                ron_string
+            }
+            Err(e) => match e {
+                NotFound => {
+                    info!("config file not found. using default config..");
+                    emit_default_config(&rule, config_path)?
+                }
+                _ => {
+                    info!("another error..");
+                    return Err(anyhow::Error::from(e));
+                }
+            },
+        })?;
     }
-
-
-    // sum 
+    // sum
     if let Some(piles) = matches.get_one::<String>("sum") {
+        info!("summing pile according to rules..");
         let mut piles_vec: Vec<u64> = Vec::new();
-
         for p in piles.split_whitespace() {
             piles_vec.push(p.parse::<u64>()?);
         }
-
         let stacks: Vec<Stack> = piles_vec.into_iter().map(|n| Stack(n)).collect();
-
-        let game = NimGame::new(RULES.to_vec(), stacks);
-
+        let game = NimGame::new([rule].to_vec(), stacks);
         println!("{}", game.calculate_nimber());
     }
 
-    if let Some(matches) = matches.subcommand_matches("test") {
-        // "$ myapp test" was run
-        if matches.get_flag("list") {
-            // "$ myapp test -l" was run
-            println!("Printing testing lists...");
-        } else {
-            println!("Not printing testing lists...");
-        }
-    }
-
     Ok(())
+}
+
+// if config file is not found
+fn emit_default_config(rule: &NimRule, path: &PathBuf) -> Result<String> {
+    let config_to_emit = ron::to_string(rule)?;
+    let comment_1 = "// configuration is in the RON format, more info can be found at https://github.com/ron-rs/ron.";
+    let comment_2 = "// the default rules are as follows. They mean you can take as many matchsticks from one pile, and piles are never";
+    let comment_3 = "// split into two or more piles. Any valid NimRule struct according to the nimlib documentation is a valid configuration.";
+    let data_to_write = vec![
+        config_to_emit,
+        comment_1.to_string(),
+        comment_2.to_string(),
+        comment_3.to_string(),
+    ];
+
+    std::fs::write(path, data_to_write.join("\n"))?;
+
+    let default_config_string = std::fs::read_to_string(PathBuf::from("./config.ron"))?;
+    Ok(default_config_string)
 }
